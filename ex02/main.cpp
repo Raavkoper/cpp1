87737e73c2b1bcab2c86d215b801e96ce319f689
Raav Koper <rkoper@Raavs-MacBook-Pro.local>
2022-09-24 12:06:45 +0200

    added ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string stringREF = string;
+
+    std::cout << "addres of the string " << &string << std::endl;
+    std::cout << "addres being held by stringPTR " << stringPTR << std::endl;
+    std::cout << "addres of the stringREF " << &stringREF << std::endl;
+
+    std::cout << "value of the string " << string << std::endl;
+    std::cout << "value being held by stringPTR " << *stringPTR << std::endl;
+    std::cout << "value of the stringREF " << stringREF << std::endl;
+}
